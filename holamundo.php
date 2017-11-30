1e7c95710254bea6133c9676fba7b273ff672307
bernanoria <31573737+bernanoria@users.noreply.github.com>
2017-11-30 10:50:03 -0300

    Update holamundo.php

--- holamundo.php
+++ holamundo.php
@@ -3,6 +3,6 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<p>!!! Hola Mundo 1234567890 !!!!! </p>'; ?>
+ <?php echo '<p>!!! Hola Mundo 123456789000000000000000000000 !!!!! </p>'; ?>
  </body>
 </html>
